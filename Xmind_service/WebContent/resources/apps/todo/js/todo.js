aa3c8406ba55503dcc8eebbbd4986a6ae88e66cf
colorpeach <514571352@qq.com>
2014-03-31 17:30:45 +0800

    fix require jQuery error

--- Xmind_service/WebContent/resources/apps/todo/js/todo.js
+++ Xmind_service/WebContent/resources/apps/todo/js/todo.js
@@ -1,9 +1,31 @@
 define(function(require,exports,module){
 	require('bootstrapcss');
-	require('jquery');
 	require('bootstrapjs');
 	require('angular');
+	
+	angular
+		.module("todo",[])
+		.controller("todoList",function($scope,$element){
+			
+			$scope.todos = [];
+			
+			$scope.addTodo = function(){
+				$scope.todos.unshift({text:$scope.text,createDate:new Date});
+				$scope.text = "";
+			};
+			
+			$scope.removeTodo = function($index){
+				$scope.todos.splice($index,1);
+			};
+			
+			$scope.todoKeyDown = function($event){
+				if($event.keyCode === 13){
+					$scope.addTodo();
+				}
+			};
+		});
+	
 	exports.run = function(){
-		console.log($,angular);
+// 		console.log($,angular);
 	};
 });
