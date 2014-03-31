084eeb990d95800288056434d2baac97633503ff
jixiangye <yjxdtc123@sina.com>
2014-03-31 23:12:56 +0800

    update test files

--- Xmind_service/src/com/mind/entity/Country.java
+++ Xmind_service/src/com/mind/entity/Country.java
@@ -1,7 +1,11 @@
 package com.mind.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 /**
@@ -14,8 +18,10 @@ public class Country implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@Column(name="CODE")
 	private String code;
 
+	@Column(name="NAME")
 	private String name;
 
 	public Country() {
