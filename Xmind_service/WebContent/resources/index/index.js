36fbffa28af5ecd199930711c31841eafe1216c9
colorpeach <514571352@qq.com>
2014-04-10 17:26:16 +0800

    Update index.js

--- Xmind_service/WebContent/resources/index/index.js
+++ Xmind_service/WebContent/resources/index/index.js
@@ -1,28 +1,37 @@
 define(function(require,exports,module){
 	require('bootstrapjs');
 	require('commonjs');
+	require('./css_img/index.css');
 	
 	angular
 		.module("app-index",["utils"])
-		.controller("loginAndRegister",['$scope','$ajax',function($scope,$ajax){
+		.controller("loginAndRegister",['$scope','$ajax','$log',function($scope,$ajax,$log){
 			var URL = {
 					LOGIN:"login/login",
 					REGISTER:"login/register"
 				};
 			
 			$scope.doLogin = function(){
-				var data = {username:$scope.loginUserName,password:$scope.loginPW};
+				var data = $scope.login;
+				
+				if($scope.loginBox.$invalid)
+					return;
+				
 				$ajax({url:URL.LOGIN,data:data,method:"post"})
 				.success(function(data){
 					
 				})
-				.fali(function(data){
+				.fail(function(data){
 					
 				});
 			};
 			
 			$scope.doRegister = function(){
-				var data = {username:$scope.regUserName,password:$scope.regPW,email:$scope.regEmail};
+				var data = $scope.reg;
+				
+				if($scope.regBox.$invalid)
+					return;
+				
 				$ajax({url:URL.REGISTER,data:data,method:"post"})
 				.success(function(data){
 					
