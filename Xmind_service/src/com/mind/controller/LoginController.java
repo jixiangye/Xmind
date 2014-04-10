d7ed3666edb87dc2f9a209746901c850c245012b
jixiangye <yjxdtc123@sina.com>
2014-04-10 21:05:13 +0800

    add session bean

--- Xmind_service/src/com/mind/controller/LoginController.java
+++ Xmind_service/src/com/mind/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.mind.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mind.bean.BaseBean;
 import com.mind.bean.ErrorBean;
+import com.mind.bean.SessionInfoBean;
 import com.mind.entity.User;
 import com.mind.service.LoginService;
+import com.mind.utils.StringUtils;
 
 @Controller
 @RequestMapping("/login")
@@ -20,10 +24,13 @@ public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public BaseBean login(@RequestBody User user) {
+	public BaseBean login(@RequestBody User user, HttpSession session) {
 		BaseBean baseBean = new BaseBean();
 		try {
 			baseBean = loginService.checkUsernameAndPassword(user);
+			if (baseBean.isSuccess()) {
+				session.setAttribute("username", user.getUsername());
+			}
 		} catch (Exception e) {
 			baseBean.setSuccess(false);
 			baseBean.getErrorBeanList().add(new ErrorBean("", "登录失败"));
@@ -42,5 +49,20 @@ public class LoginController {
 			baseBean.getErrorBeanList().add(new ErrorBean("", "注册失败"));
 		}
 		return baseBean;
+	}
+
+	@RequestMapping(value = "/getSessionInfo", method = RequestMethod.POST)
+	@ResponseBody
+	public SessionInfoBean getSessionInfo(HttpSession session) {
+		SessionInfoBean sessionInfoBean = new SessionInfoBean();
+		try {
+			sessionInfoBean.setUsername(StringUtils.checkNull(session
+					.getAttribute("username")));
+		} catch (Exception e) {
+			sessionInfoBean.setSuccess(false);
+			sessionInfoBean.getErrorBeanList().add(
+					new ErrorBean("", "获取session失败"));
+		}
+		return sessionInfoBean;
 	}
 }
